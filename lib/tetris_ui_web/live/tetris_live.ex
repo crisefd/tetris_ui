82cbdfbec016af0b83febc05e912318c6b72299f
Cristhian Fuertes <cristhian.e.fuertes@gmail.com>
2020-01-09 17:53:15 -0500

    basic rendering of a tetromino

--- lib/tetris_ui_web/live/tetris_live.ex
+++ lib/tetris_ui_web/live/tetris_live.ex
@@ -2,44 +2,85 @@ defmodule TetrisUiWeb.TetrisLive do
   use Phoenix.LiveView
   import Phoenix.HTML, only: [raw: 1]
   alias Tetris.Brick
+  alias Tetris.Shape
   alias TetrisUi.Shades
 
-  @type shades :: Shades.t
-  @type point :: Brick.point
-
+  @type shades :: Shades.t()
+  @type point :: Brick.point()
+  @type color :: Brick.color()
+  @type shape :: Shape.t()
 
   @box_height 20
   @box_width 20
+
+  # @type color :: :red | :blue | :green | :orange | :grey
 
   @shades_list [
     %Shades{light: "DB7160", dark: "AB574B"},
     %Shades{light: "83C1C8", dark: "66969C"},
     %Shades{light: "8BBF57", dark: "769359"},
     %Shades{light: "CB8E4E", dark: "AC7842"},
-    %Shades{light: "A1A09E", dark: "7F7F7E"},
-  ]
+    %Shades{light: "A1A09E", dark: "7F7F7E"}
+  ] 
 
   def mount(_session, socket) do
-    {
-      :ok,
-      assign(socket,
-            tetromino: Brick.new(:random) |> Brick.to_string())
-    }
+    { :ok, new_game(socket) }
   end
 
   def render(assigns) do
-    ~L""" 
+    ~L"""
       <h1>Tetromino</h1>
-      <pre><%= @tetromino %></pre>
       <div> 
       <%= raw svg_header() %>
-      <%= raw box({1, 1}, :red) %> 
+      <%= raw boxes(@tetromino) %> 
       <%= raw svg_footer() %>
       </div>
     """
   end
 
+  def new_game(socket) do
+    socket
+    |> assign( state: :playing, score: 0)
+    |> display
+  end
+
+  def display(socket) do
+    socket
+    |> new_brick
+    |> new_tetromino
+  end
+
+  def new_brick(socket) do
+    brick = 
+      Brick.new(:random)
+      |> Map.put(:location, {3, 1})
+
+    assign(socket, brick: brick)
+  end
+
+  def new_tetromino(socket = %{assigns: %{brick: brick}}) do
+    brick_color = Brick.color brick
+
+    shape =
+      brick
+      |> Brick.prepare()
+      |> Shape.with_color(brick_color)
+
+    assign(socket, tetromino: shape)
+  end
+
+  @spec boxes(shape) :: binary
+
+  def boxes(colored_shape) do
+    colored_shape
+    |> Enum.map(fn {x, y, color} ->
+      box({x, y}, color)
+    end)
+    |> Enum.join("\n")
+  end
+
   @spec svg_header :: binary
+
   def svg_header do
     """
       <svg
@@ -57,18 +98,19 @@ defmodule TetrisUiWeb.TetrisLive do
 
   def svg_footer, do: "</svg>"
 
-  @spec square(point, binary) :: binary 
+  @spec square(point, binary) :: binary
 
   def square(point, shade) do
-   {x, y} = to_pixels(point)
-   {w, h} = {@box_width, @box_height}
-   """
-    <rect
-    x="#{x + 1}" y="#{y + 1}"
-    style="fill:##{shade};"
-    width="#{w - 2}" height="#{h - 2}"
-    />
-   """
+    {x, y} = to_pixels(point)
+    {w, h} = {@box_width, @box_height}
+
+    """
+     <rect
+     x="#{x + 1}" y="#{y + 1}"
+     style="fill:##{shade};"
+     width="#{w - 2}" height="#{h - 2}"
+     />
+    """
   end
 
   @spec triangle(point, binary) :: binary
@@ -76,23 +118,23 @@ defmodule TetrisUiWeb.TetrisLive do
   def triangle(point, shade) do
     {x, y} = to_pixels(point)
     {w, h} = {@box_width, @box_height}
+
     """
       <polyline
       style="fill:##{shade};"
       points="#{x + 1}, #{y + 1}  #{x + w}, #{y + 1}  #{x + w},  #{y + h}"
       />
     """
-   end
+  end
 
-   @spec box(point, binary) :: binary
+  @spec box(point, color) :: binary
 
-   def box(point, color) do
-
-     """
-      #{square(point, shades(color).light)}
-      #{triangle(point, shades(color).dark)}
-     """
-   end
+  def box(point, color) do
+    """
+     #{square(point, shades(color).light)}
+     #{triangle(point, shades(color).dark)}
+    """
+  end
 
   @spec to_pixels(point) :: point
 
@@ -100,11 +142,10 @@ defmodule TetrisUiWeb.TetrisLive do
 
   @spec shades(atom) :: shades
 
-  for {shades, color} <- @shades_list 
-                        |> Enum.map(&Macro.escape/1)
-                        |> Enum.zip(Brick.all_colors) do
+  for {shades, color} <-
+        @shades_list
+        |> Enum.map(&Macro.escape/1)
+        |> Enum.zip(Brick.all_colors()) do
     defp shades(unquote(color)), do: unquote(shades)
-
   end
-
 end
